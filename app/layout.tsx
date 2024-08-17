ece7f56eec6400de8854626995ea6fbf87228fcd
Sagar Chand <chandsagar314@gmail.com>
2024-08-17 21:00:57 +0545

    changes in images

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider
+          appearance={{
+            layout: {
+              socialButtonsVariant: "iconButton",
+              logoImageUrl: "/logo.svg",
+            },
+            variables: {
+              colorBackground: "#15171c",
+              colorPrimary: "",
+              colorText: "white",
+              colorInputBackground: "#1b1f29",
+              colorInputText: "white",
+            },
+          }}
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+        >
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
